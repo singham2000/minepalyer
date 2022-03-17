e4930c0b14e7eeb94f637c823aa9a697c3337286
surya pratap <thakursuryapratap9@gmail.com>
2022-03-17 23:16:51 +0530

    d

---
+++ bot.js
@@ -0,0 +1,28 @@
+const mineflayer = require("mineflayer");
+
+var settings = {
+    username: "GOD",
+    host: "Singham2000.aternos.me",
+};
+
+const bot = mineflayer.createBot(settings);
+
+bot.once("spawn", ()=>{
+    bot.chat("/give @a diamonds 50");
+});
+
+bot.on("move", ()=>{
+    let friend = bot.nearestEntity();
+
+    if (friend) {
+        bot.lookAt(friend.position.offset(0, friend.height, 0));
+    }
+});
+
+var walking = false;
+
+bot.on("entityHurt", (entity)=>{
+    if (entity != bot.entity) return;
+    walking = !walking;
+    bot.setControlState("forward", walking);
+});
